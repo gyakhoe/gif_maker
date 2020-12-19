46daa2213e8a523a8f296e3e2e56a9fa890fe16b
Gyague Sonam <gyakhoe@gmail.com>
2020-12-19 11:55:12 +0545

    Home UI modified and List view basic added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:gif_maker/views/gif_maker_home.dart';
+import 'package:gif_maker/views/gif_maker_main_view.dart';
+import 'package:gif_maker/views/home_view.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,13 +10,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: GifMakerHome(),
-    );
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: HomeView());
   }
 }
